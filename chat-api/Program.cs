5c5159f6a294c3020291d80dba07e8e296a6f35f
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-10-13 11:36:40 +0200

    Grund för servern tillsammans med signalr har satts upp

---
+++ chat-api/Program.cs
@@ -0,0 +1,48 @@
+using ChatAPI.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
+{
+    builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials()
+        .WithOrigins("http://localhost:5000");
+}));
+
+builder.Services.AddSignalR(o =>
+{
+    if (!builder.Environment.IsProduction())
+    {
+        o.EnableDetailedErrors = true;
+    }
+});
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHub<ChatHub>("/chathub");
+
+app.Run();
